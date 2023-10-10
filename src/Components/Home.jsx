d0e9099640970ece7dd266062816363b23140ab3
Esteban Sepulveda <tebansd@icloud.com>
2023-10-10 14:26:09 -0700

    fixed navbar position

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,11 @@
 import HeroSection from "./Hero";
+import MySkills from "./Skills";
 
 export default function Home() {
     return (
         <>
          <HeroSection />
+         <MySkills />
         </>
     )
 }
