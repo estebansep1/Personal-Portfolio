169dc25035f72d51bdf2d6d18efd4a08c978beb1
Esteban Sepulveda <tebansd@icloud.com>
2023-10-09 22:02:18 -0700

    linked email to button

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -13,7 +13,11 @@ export default function HeroSection() {
             I am a junior developer currently enrolled in the SDSU Software Development Bootcamp, actively working to expand my skills and knowledge in Sofware Development
             </p>
           </div>
-          <button className="btn btn-primary">Get In Touch</button>
+          <button className="btn btn-primary">
+            <a href="mailto:tebansd@icloud.com" style={{ textDecoration: 'none', color: 'inherit' }}>
+                Get In Touch
+            </a>
+          </button>
         </div>
         <div className="hero--section--img">
           <img src="./logo.png" alt="Hero Section" />
