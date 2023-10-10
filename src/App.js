7e8e88f3590933fc48df2af0f28577111e852e64
Esteban Sepulveda <tebansd@icloud.com>
2023-10-09 22:37:10 -0700

    fixed button/added skeleton for navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from'react-router-dom';
 import Home from './Components/Home';
+import NavBar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Home />
     </div>
   );
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -30,11 +30,10 @@ export default function HeroSection() {
             I am a junior developer currently enrolled in the SDSU Software Development Bootcamp, actively working to expand my skills and knowledge in Software Development
           </p>
         </div>
-        <button className="btn btn-primary">
-          <a href="mailto:tebansd@icloud.com" style={{ textDecoration: 'none', color: 'inherit' }}>
-            Get In Touch
-          </a>
+        <button className="btn btn-primary" onClick={() => window.location.href = "mailto:tebansd@icloud.com"}>
+          Get In Touch
         </button>
+
       </div>
       <div className="hero--section--img">
         <img src="./logo.png" alt="Hero Section" />
