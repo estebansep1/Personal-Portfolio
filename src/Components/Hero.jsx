1b6919bc34a8d68fec877c79e8c5990cf50c0081
Esteban Sepulveda <tebansd@icloud.com>
2023-10-09 22:22:05 -0700

    added typed.js functionality

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,27 +1,44 @@
+import React, { useEffect } from 'react';
+import Typed from 'typed.js';
+
 export default function HeroSection() {
-    return (
-      <section id="heroSection" className="hero--section">
-        <div className="hero--section--content--box">
-          <div className="hero--section--content">
-            <p className="section--title">Hey, I'm Esteban</p>
-            <h1 className="hero--section--title">
-              <span className="hero--section-title--color">Full Stack</span>{" "}
-              <br />
-              Developer
-            </h1>
-            <p className="hero--section-description">
-            I am a junior developer currently enrolled in the SDSU Software Development Bootcamp, actively working to expand my skills and knowledge in Sofware Development
-            </p>
-          </div>
-          <button className="btn btn-primary">
-            <a href="mailto:tebansd@icloud.com" style={{ textDecoration: 'none', color: 'inherit' }}>
-                Get In Touch
-            </a>
-          </button>
+  useEffect(() => {
+    const typed = new Typed('.hero--section-title', {
+      strings: ['Full Stack Developer', 'Software Devloper', 'Student'],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 1000,
+      loop: true,
+      showCursor: false,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Esteban</p>
+          <h1 className="hero--section-title">
+            <span className="hero--section-title--color">Full Stack</span> <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            I am a junior developer currently enrolled in the SDSU Software Development Bootcamp, actively working to expand my skills and knowledge in Software Development
+          </p>
         </div>
-        <div className="hero--section--img">
-          <img src="./logo.png" alt="Hero Section" />
-        </div>
-      </section>
-    );
-  }
+        <button className="btn btn-primary">
+          <a href="mailto:tebansd@icloud.com" style={{ textDecoration: 'none', color: 'inherit' }}>
+            Get In Touch
+          </a>
+        </button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./logo.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
