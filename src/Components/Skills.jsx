6cfcbcc69a1581c2b1d264d1f47d746f544fe7eb
Esteban Sepulveda <tebansd@icloud.com>
2023-10-11 22:43:20 -0700

    started mySkills

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 
 export default function MySkills() {
     return (
-     <div>
-        <h1>Skills</h1>
-     </div>
+     <section className="skills--section"
+     id="mySkills">
+        <div className="portfolio--container">
+            <p className="section--title">My Skills</p>
+            <h2 className="skills--section--heading">Technical Skills</h2>
+        </div>
+        <div className="skills--section--container">
+            <div className="skills--section--card">
+                <div className="skills--section--img">
+                    <img src="./images/frontend.png" alt="frontend" />
+                </div>
+            </div>
+        </div>
+     </section>
     )
 }
