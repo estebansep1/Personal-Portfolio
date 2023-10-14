a424865d5ac27c0702c4f9ddeffb832a97ba9fbe
Esteban Sepulveda <tebansd@icloud.com>
2023-10-14 14:09:00 -0700

    added aboutme description and title

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -3,7 +3,15 @@ export default function AboutMe() {
         <div>
             <section id="AboutMe" className="about--section">
                 <div className="about--section--img">
-                    
+                    <img src="./images/profile.png" alt="About Me" />
+                </div>
+                <div className="hero--section--content--box about--section--box">
+                    <div className="hero--section--content">
+                        <p className="section--title">About</p>
+                        <h1 className="skills--section--heading">About Me</h1>
+                        <p className="hero--section-description">Hello, I'm Esteban, and I'm currently a student at SDSU's Software Development Bootcamp. I'm passionate about coding and dedicated to learning and growing in the world of software development.</p>
+                        <p className="hero--section-description">During my journey, I'm acquiring skills in areas like Front-end and Back-End technologies. I'm excited to combine my education with real-world experience and build amazing projects. Feel free to reach out to connect, collaborate, or just chat about technology!</p>
+                    </div>
                 </div>
             </section>
         </div>
