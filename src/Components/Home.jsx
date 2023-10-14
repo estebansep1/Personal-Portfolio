9baf44fe9d74504d55b645f5cf05de5869f0bf48
Esteban Sepulveda <tebansd@icloud.com>
2023-10-13 22:37:11 -0700

    Implemented about me skeleton component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "./Hero";
 import MySkills from "./Skills";
+import AboutMe from "./AboutMe";
 
 export default function Home() {
     return (
         <>
          <HeroSection />
          <MySkills />
+         <AboutMe />
         </>
     )
 }
