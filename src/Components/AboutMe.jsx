2c0e7e70e3e20ef2dff623710d8e25e9ffc9a858
Esteban Sepulveda <tebansd@icloud.com>
2023-10-15 20:38:59 -0700

    fixed some wording

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -9,7 +9,7 @@ export default function AboutMe() {
                     <div className="hero--section--content">
                         <p className="section--title">About</p>
                         <h1 className="skills--section--heading">About Me</h1>
-                        <p className="hero--section-description">Hello, I'm Esteban, and I'm currently a student at SDSU's Software Development Bootcamp. I'm passionate about coding and dedicated to learning and growing in the world of software development.</p>
+                        <p className="hero--section-description">Hello, I'm Esteban, and I'm currently a student at San Diego State's Software Development Bootcamp. I'm passionate about coding and dedicated to learning and growing in the world of software development.</p>
                         <p className="hero--section-description">During my journey, I'm acquiring skills in areas like Front-end and Back-End technologies. I'm excited to combine my education with real-world experience and build amazing projects. Feel free to reach out to connect, collaborate, or just chat about technology!</p>
                     </div>
                 </div>
