9ce55e1684059d777d72a120e7a772667129cdc8
Esteban Sepulveda <tebansd@icloud.com>
2023-10-16 12:22:26 -0700

    added data file for projects and first project screenshot

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,7 +1,17 @@
-export default function MyPortfolio() {
-    return (
-        <div>
-            <h1>My Portfolio</h1>
+import React from 'react';
+import projects from './Project-Data/projects';
+
+const MyPortfolio = () => {
+  return (
+    <section id="MyPortfolio" className="portfolio--section">
+      <div className="portfolio--container--box">
+        <div className="portfolio--container">
+          <p className="sub--title">Projects</p>
+          <h2 className="section--heading">My Portfolio</h2>
         </div>
-    )
+      </div>
+    </section>
+  );
 }
+
+export default MyPortfolio;
---
+++ src/Components/Project-Data/projects.js
@@ -0,0 +1,11 @@
+const projects = [
+    {
+        id: 1,
+        title: 'Game of Pong',
+        description: `This is a classic Pong game where two players control paddles to hit a ball back and forth, trying to score points by passing the ball past the opponent's paddle.`,
+        imgUrl: '/images/game-of-pong.png',
+        githubUrl: 'https://github.com/estebansep1/PongGame'
+    }
+]
+
+export default projects;
