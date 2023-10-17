913adfbcad10a37f05d9aa27193fa0d597d83507
Esteban Sepulveda <tebansd@icloud.com>
2023-10-17 15:44:38 -0700

    added hover styling to portfolio and added footer skeleton

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,6 +2,7 @@ import HeroSection from "./Hero";
 import MySkills from "./Skills";
 import AboutMe from "./AboutMe";
 import MyPortfolio from "./Portfolio";
+import Footer from "./Footer";
 
 export default function Home() {
     return (
@@ -10,6 +11,7 @@ export default function Home() {
          <MySkills />
          <AboutMe />
          <MyPortfolio />
+         <Footer />
         </>
     )
 }
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -57,6 +57,24 @@ export default function MyPortfolio() {
                   />
                 </svg>
               </a>
+              <a href="https://gameofpong.netlify.app/" className="text-sm portfolio--link">
+                View Deployed Game
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 20 19"
+                  fill="none"
+                >
+                  <path
+                    d="M4.66667 1.66675H18V15.0001M18 1.66675L2 17.6667L18 1.66675Z"
+                    stroke="currentColor"
+                    stroke-width="2.66667"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </a>
             </div>
           </div>
         ))}
