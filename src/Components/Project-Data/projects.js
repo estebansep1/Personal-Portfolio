16de8a356d8b73976ffa23ac5d8e91620aa3a3a7
Esteban Sepulveda <tebansd@icloud.com>
2023-10-17 12:34:29 -0700

    finished portfolio for now

--- src/Components/Project-Data/projects.js
+++ src/Components/Project-Data/projects.js
@@ -3,9 +3,9 @@ const data = {
       {
         id: 1,
         title: 'Game of Pong',
-        description: `This is a classic Pong game where two players control paddles to hit a ball back and forth, trying to score points by passing the ball past the opponent's paddle. Game was made with HTML, CSS, and vanilla Javascript.`,
-        src: '/images/game-of-pong.png',
-        link: 'https://github.com/estebansep1/PongGame'
+        description: `This is a classic Pong game where two players control paddles to hit a ball back and forth, trying to score points by passing the ball past the opponent's paddle. Game was made with HTML, CSS, and vanilla JavaScript.`,
+        src: '/images/game-of-pong.svg',
+        link: 'View GitHub Repository'
       },
     ],
   };
