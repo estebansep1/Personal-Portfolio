aee7ec505de549c76ccdb265b51d788f87085da8
Esteban Sepulveda <tebansd@icloud.com>
2023-10-16 21:21:16 -0700

    added project button. need links

--- src/Components/Project-Data/projects.js
+++ src/Components/Project-Data/projects.js
@@ -1,11 +1,13 @@
-const projects = [
-    {
+const data = {
+    portfolio: [
+      {
         id: 1,
         title: 'Game of Pong',
-        description: `This is a classic Pong game where two players control paddles to hit a ball back and forth, trying to score points by passing the ball past the opponent's paddle.`,
-        imgUrl: '/images/game-of-pong.png',
-        githubUrl: 'https://github.com/estebansep1/PongGame'
-    }
-]
-
-export default projects;
+        description: `This is a classic Pong game where two players control paddles to hit a ball back and forth, trying to score points by passing the ball past the opponent's paddle. Game was made with HTML, CSS, and vanilla Javascript.`,
+        src: '/images/game-of-pong.png',
+        link: 'https://github.com/estebansep1/PongGame'
+      },
+    ],
+  };
+  
+  export default data;
