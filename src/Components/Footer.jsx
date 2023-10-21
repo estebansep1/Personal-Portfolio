61566a0efeca100553e4e96757da935976ce666b
Esteban Sepulveda <tebansd@icloud.com>
2023-10-20 22:13:45 -0700

    implemented footer logo

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,14 @@
+import React from "react"
+import { Link } from "react-scroll"
+
 export default function Footer() {
     return (
-        <div>
-            <h1>Footer</h1>
-        </div>
+        <footer className="className">
+            <div className="footer-link--container">
+                <div>
+                    <img src="./images/name.svg" alt="Esteban" />
+                </div>
+            </div>
+        </footer>
     )
 }
