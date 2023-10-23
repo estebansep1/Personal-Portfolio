f04ec9feb435eb338895a46e4738c7f404d3506a
Esteban Sepulveda <tebansd@icloud.com>
2023-10-23 11:56:10 -0700

    implemented footer links

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,6 +8,54 @@ export default function Footer() {
                 <div>
                     <img src="./images/name.svg" alt="Esteban" />
                 </div>
+                <div className="footer--items">
+                    <ul>
+                        <li>
+                            <Link
+                              activeClass="navbar--active-content"
+                              spy={true}
+                              smooth={true}
+                              offset={-70}
+                              duration={500}
+                              to="heroSection"
+                              className="text-md"
+                              >Home</Link>
+                        </li>
+                        <li>
+                            <Link
+                              activeClass="navbar--active-content"
+                              spy={true}
+                              smooth={true}
+                              offset={-70}
+                              duration={500}
+                              to="mySkills"
+                              className="text-md"
+                              >Skills</Link>
+                        </li>
+                        <li>
+                            <Link
+                              activeClass="navbar--active-content"
+                              spy={true}
+                              smooth={true}
+                              offset={-70}
+                              duration={500}
+                              to="AboutMe"
+                              className="text-md"
+                              >About Me</Link>
+                        </li>
+                        <li>
+                            <Link
+                              activeClass="navbar--active-content"
+                              spy={true}
+                              smooth={true}
+                              offset={-70}
+                              duration={500}
+                              to="MyPortfolio"
+                              className="text-md"
+                              >Portfolio</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </footer>
     )
