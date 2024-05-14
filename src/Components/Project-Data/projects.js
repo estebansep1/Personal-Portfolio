d38e9b9e86af1c7bc36b73fa2a833e18951e7c89
Esteban Sepulveda <tebansd@icloud.com>
2024-05-14 14:08:19 -0700

    fixed project desc

--- src/Components/Project-Data/projects.js
+++ src/Components/Project-Data/projects.js
@@ -11,7 +11,7 @@ const data = {
     {
       id: 2,
       title: 'Cuisine Shuffle',
-      description: `A fun web application designed for my family and friends to help us decide where to eat in our area. By entering a location, the app shuffles and displays restaurant options, making the decision-making process easier and more enjoyable.`,
+      description: `A fun web application designed for my family and friends to help us decide where to eat in our area. The app shuffles and displays restaurant options, making the decision-making process easier and more enjoyable.`,
       src: ['/images/cuisineshuffle.svg', '/images/cuisineshuffle(1).svg', '/images/cuisineshuffle(2).svg'],
       githubLink: 'https://github.com/estebansep1/Cuisine-Shuffle',
       liveLink: 'https://main--cuisineshuffle.netlify.app/',
