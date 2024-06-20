d95a6e2c0d64aecf73c6196622c7841e7a31cc2a
Esteban Sepulveda <tebansd@icloud.com>
2024-06-20 14:19:04 -0700

    added new feature for mobile users where in hero section the typed js animationgets replaced with a gsap animation to fix the content collapse issue on mobile

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,34 +1,44 @@
 import React, { useEffect } from "react";
 import Typed from "typed.js";
+import { useMediaQuery } from 'react-responsive';
+import SkillDisplay from "./Mobile/MobileSkillsDisplay";
 
 export default function HeroSection() {
-  useEffect(() => {
-    const typed = new Typed(".hero--section-title", {
-      strings: ["Full Stack Developer", "Web Developer", "Lifelong Learner"],
-      typeSpeed: 100,
-      backSpeed: 100,
-      backDelay: 1000,
-      loop: true,
-      showCursor: false,
-    });
+  const isMobile = useMediaQuery({ maxWidth: 1200 });
 
-    return () => {
-      typed.destroy();
-    };
-  }, []);
+  useEffect(() => {
+    if (!isMobile) {
+      const typed = new Typed(".hero--section-title", {
+        strings: ["Full Stack Developer", "Web Developer", "Lifelong Learner"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true,
+        showCursor: false,
+      });
+
+      return () => {
+        typed.destroy();
+      };
+    }
+  }, [isMobile]);
 
   return (
     <section id="heroSection" className="hero--section">
       <div className="hero--section--content--box">
         <div className="hero--section--content">
           <p className="section--title">Hey, I'm Esteban</p>
-          <h1 className="hero--section-title">
-            <span className="hero--section-title--color">Full Stack</span> <br />
-            Developer
-          </h1>
+          {isMobile ? (
+            <SkillDisplay />
+          ) : (
+            <h1 className="hero--section-title">
+              <span className="hero--section-title--color">Full Stack</span> <br />
+              Developer
+            </h1>
+          )}
           <p className="hero--section-description">
-          As a junior developer with a strong foundation in web development, I recently graduated from a software development bootcamp. 
-          My expertise is rooted in software engineering, where I am continuously seeking to deepen my knowledge and skills in this dynamic field.
+            As a junior developer with a strong foundation in web development, I recently graduated from a software development bootcamp. 
+            My expertise is rooted in software engineering, where I am continuously seeking to deepen my knowledge and skills in this dynamic field.
           </p>
         </div>
         <a href="mailto:tebansd@icloud.com" className="btn btn-primary">
---
+++ src/Components/Mobile/MobileSkillsDisplay.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState, useRef } from 'react';
+import { gsap } from 'gsap';
+import '../Mobile/MobileSkillsDisplay.scss';
+
+const skills = ['JavaScript', 'React', 'MongoDB' , 'CSS', 'Node', 'Express', 'Git', 'Netlify', 'Render', 'AWS'];
+
+const SkillDisplay = () => {
+  const [skillIndex, setSkillIndex] = useState(0);
+  const element = useRef(null);
+
+  useEffect(() => {
+    const tl = gsap.timeline({ repeat: -1, repeatDelay: 1 });
+    tl.to(element.current, { autoAlpha: 1, duration: 0.8 })
+      .to(element.current, { autoAlpha: 1, duration: 1.2 })
+      .to(element.current, { autoAlpha: 0, duration: 0.8, onComplete: () => {
+          setSkillIndex((prevIndex) => (prevIndex + 1) % skills.length);
+      } });
+
+    return () => {
+      tl.kill();
+    };
+  }, [skillIndex]);
+
+  return (
+    <div className="skill-display">
+      <div ref={element}>
+        {skills[skillIndex]}
+      </div>
+    </div>
+  );
+};
+
+export default SkillDisplay;
