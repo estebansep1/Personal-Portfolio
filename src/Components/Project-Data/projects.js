670804e9fd83f67ef24383bd8e71951d1957e950
Esteban Sepulveda <tebansd@icloud.com>
2024-06-25 12:47:28 -0700

    fixed the project picture order for game of pong to include the homepage first

--- src/Components/Project-Data/projects.js
+++ src/Components/Project-Data/projects.js
@@ -4,7 +4,7 @@ const data = {
       id: 1,
       title: 'Game of Pong',
       description: `This is a classic Pong game where two players control paddles to hit a ball back and forth, trying to score points by passing the ball past the opponent's paddle. Game was made with HTML, CSS, and vanilla JavaScript.`,
-      src: ['/images/game-of-pong.svg', '/images/pong-win.svg', '/images/pong-home.svg'],
+      src: ['/images/pong-home.svg', '/images/game-of-pong.svg', '/images/pong-win.svg'],
       githubLink: 'https://github.com/estebansep1/PongGame',
       liveLink: 'https://gameofpong.netlify.app/',
     },
